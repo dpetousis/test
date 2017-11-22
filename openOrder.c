e7cfb8500c67cbbf315471827c61382dffce72c3
dpetousis <dpetousis@gmail.com>
2017-11-22 15:35:24 +0000

    Update openOrder.c

--- openOrder.c
+++ openOrder.c
@@ -1,24 +1,28 @@
 //--------------------------------------------------------------------
 // openOrder.mq4 
 //--------------------------------------------------------------- 1 --
+
+#property script_show_confirm
+#property script_show_inputs
+
 int start()                       
   {
-
-
-
-	  int magic = 9999;
-	  double slowFilter = -1.0;
-	  double loss = 0;
-	  double sequence = 0;
-	  string name = "EURUSDpro";
-	  int orderType = OP_BUYLIMIT; 		
-	  double lots = 0.01;
-	  double SL = 1.18;
-	  double TP = 1.22;
-	  string price = MarketInfo(name,MODE_ASK);		// if BUY use ASK
-      string s_comment = StringConcatenate(IntegerToString(magic),"_",DoubleToStr(slowFilter,(int)MarketInfo(name,MODE_DIGITS)),"_",DoubleToStr(loss,2),"_",DoubleToStr(sequence,0));
-	  ticket=OrderSend(name,orderType,lots,price,10,SL,TP,s_comment,magic); //Opening 
-                                         
+	 
+	extern int ticket = 0;
+	res = OrderSelect(ticket,SELECT_BY_TICKET);
+	if (res) {
+	  string name = OrderSymbol();
+	  int orderType = OrderType(); 
+	  string price;
+	  if (orderType = OP_BUYLIMIT) {
+	  	price = MarketInfo(name,MODE_ASK);		// if BUY use ASK
+	  }
+	  elseif (orderType = OP_SELLLIMIT) {
+		price = MarketInfo(name,MODE_BID);
+	  }
+      	ticket=OrderSend(name,orderType,OrderLots(),price,10,OrderStopLoss(),OrderTakeProfit(),OrderComment(),OrderMagicNumber()); //Opening 
+	}
+	else { Alert("Ticket: ", ticket, " cannot be selected."); }
 
 
    return;                          
